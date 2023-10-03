f8d3a2fbeba288324c22917de6b69eb09e8004d9
VDani3 <villacarmona.dani@gmail.com>
2023-10-03 20:31:35 +0200

    Elecctrodomestics acabats

---
+++ Project/src/Nevera.java
@@ -0,0 +1,20 @@
+public class Nevera extends Electrodomestic {
+    private int frigories;
+    private String soroll;
+
+    public Nevera(){};
+
+    public Nevera(Nevera target){
+        super(target);
+        if (target != null){
+            this.frigories = target.frigories;
+            this.soroll = target.soroll;
+        }
+    }
+
+    @Override
+    public Electrodomestic clone() {
+       return new Nevera(this);
+    }
+    
+}
